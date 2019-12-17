dd283ba7c0884fa70b250c420f3268c8f3d4d051
Rana AITS <ranaaits@Tuhins-MacBook-Pro.local>
2019-12-17 16:13:47 +0600

    tab Bar added

---
+++ Take Notes/CreateNoteViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CreateNoteViewController.swift
+//  Take Notes
+//
+//  Created by Rana AITS on 12/17/19.
+//  Copyright © 2019 Rana AITS. All rights reserved.
+//
+
+import UIKit
+
+class CreateNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+   
+
+}
---
+++ Take Notes/NoteViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NoteViewController.swift
+//  Take Notes
+//
+//  Created by Rana AITS on 12/17/19.
+//  Copyright © 2019 Rana AITS. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+
+
+}
