73ce49b21ad15f765ffa26149abc870f25e0b9bb
Rana AITS <ranaaits@Tuhins-MacBook-Pro.local>
2019-12-17 14:30:34 +0600

    intial commit

---
+++ Take Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Take Notes
+//
+//  Created by Rana AITS on 12/17/19.
+//  Copyright © 2019 Rana AITS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
