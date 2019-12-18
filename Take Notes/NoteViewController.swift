85d7aea964ce8e2e78e516610a626240e620a854
Rana AITS <ranaaits@Tuhins-MacBook-Pro.local>
2019-12-18 16:52:22 +0600

    remove and update work
    Signed-off-by: Rana AITS <ranaaits@Tuhins-MacBook-Pro.local>

--- Take Notes/NoteViewController.swift
+++ Take Notes/NoteViewController.swift
@@ -10,55 +10,59 @@ import UIKit
 import FirebaseDatabase
 
 class NoteViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
     var databaseHandle:DatabaseHandle?
     var ref:DatabaseReference?
     var noteData = [String]()
     
-    
+    var key = [String]()
     
     var text:String = ""
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.reloadData()
         ref = Database.database().reference()
-          
+        
         databaseHandle = ref?.child("Notes").observe(.childAdded, with: { (snapshot) in
             //to execute when a child is added under notes
             //take the value from snapshot and put into notedata array
             let note = snapshot.value as? String
+            let key = snapshot.key
+            print("Key  : ",key)
+            
             print("Added: \(note!)")
             if let actualNote = note {
-            self.noteData.append(actualNote)
-            self.tableView.reloadData()
+                self.noteData.append(actualNote)
+                self.key.append(key)
+                self.tableView.reloadData()
             }
         })
         
-       // ref?.child("Notes").childByAutoId().setValue(text)
+        // ref?.child("Notes").childByAutoId().setValue(text)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return noteData.count
-      }
-      
-      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
         
         let note = noteData[indexPath.row]
         cell.titleLabel.text = note
         return cell
-      }
+    }
     
-     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
-
-     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         let editAction = UITableViewRowAction(style: .default, title: "Edit", handler: { (action, indexPath) in
             let alert = UIAlertController(title: "", message: "Edit item", preferredStyle: .alert)
             alert.addTextField(configurationHandler: { (textField) in
@@ -66,56 +70,65 @@ class NoteViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
             })
             alert.addAction(UIAlertAction(title: "Update", style: .default, handler: { (updateAction) in
                 self.noteData[indexPath.row] = alert.textFields!.first!.text!
+                
+                let txt = self.noteData[indexPath.row]
+//                self.ref?.child("Notes").childByAutoId().setValue(txt)
+                let keyid = self.key[indexPath.row]
+                self.ref?.child("Notes").child(keyid).setValue(txt)
                 self.tableView.reloadRows(at: [indexPath], with: .fade)
+                
             }))
             alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
             self.present(alert, animated: false)
         })
-
+        
         let deleteAction = UITableViewRowAction(style: .default, title: "Delete", handler: { (action, indexPath) in
             self.noteData.remove(at: indexPath.row)
+            let keyid = self.key[indexPath.row]
+            print("delete:",keyid)
+            self.ref?.child("Notes").child(keyid).removeValue()
             tableView.reloadData()
         })
-
+        
         return [deleteAction, editAction]
     }
-
     
-      func alertWithTF() {
-          //Step : 1
-          let alert = UIAlertController(title: "Notes", message: "create notes", preferredStyle: UIAlertController.Style.alert )
-          //Step : 2
-          let save = UIAlertAction(title: "Save", style: .default) { (alertAction) in
-              let textField = alert.textFields![0] as UITextField
-              if textField.text != "" {
-                  //Read TextFields text data
-                  self.text = textField.text!
-                  self.ref?.child("Notes").childByAutoId().setValue(self.text)
-                  print("TF  : \(textField.text!)")
-              } else {
-                  print("TF is Empty...")
-              }
-          }
-          //Step : 3
-          //For first TF
-          alert.addTextField { (textField) in
-              textField.placeholder = "Enter your notes here"
-              textField.textColor = .red
-          }
-          //Step : 4
-          alert.addAction(save)
-          //Cancel action
-          let cancel = UIAlertAction(title: "Cancel", style: .default) { (alertAction) in }
-          alert.addAction(cancel)
-          //OR single line action
-          //alert.addAction(UIAlertAction(title: "Cancel", style: .default) { (alertAction) in })
-
-          self.present(alert, animated:true, completion: nil)
-
-      }
+    
+    func alertWithTF() {
+        //Step : 1
+        let alert = UIAlertController(title: "Notes", message: "create notes", preferredStyle: UIAlertController.Style.alert )
+        //Step : 2
+        let save = UIAlertAction(title: "Save", style: .default) { (alertAction) in
+            let textField = alert.textFields![0] as UITextField
+            if textField.text != "" {
+                //Read TextFields text data
+                self.text = textField.text!
+                self.ref?.child("Notes").childByAutoId().setValue(self.text)
+                print("TF  : \(textField.text!)")
+            } else {
+                print("TF is Empty...")
+            }
+        }
+        //Step : 3
+        //For first TF
+        alert.addTextField { (textField) in
+            textField.placeholder = "Enter your notes here"
+            textField.textColor = .red
+        }
+        //Step : 4
+        alert.addAction(save)
+        //Cancel action
+        let cancel = UIAlertAction(title: "Cancel", style: .default) { (alertAction) in }
+        alert.addAction(cancel)
+        //OR single line action
+        //alert.addAction(UIAlertAction(title: "Cancel", style: .default) { (alertAction) in })
+        
+        self.present(alert, animated:true, completion: nil)
+        
+    }
     
     @IBAction func createNew(_ sender: UIBarButtonItem) {
-           
-           alertWithTF()
-       }
+        
+        alertWithTF()
+    }
 }
